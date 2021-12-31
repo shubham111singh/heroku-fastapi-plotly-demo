069038bd288119d4bce0f0d949770e8e95794270
shubham111singh <77665496+shubham111singh@users.noreply.github.com>
2021-12-31 10:32:57 +0530

    Update main.py
    creating log files

--- src/collatz_webservice/main.py
+++ src/collatz_webservice/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,Request
+from datetime import datetime
 from .webservice.utils import NoUvicornException
 from .webservice.routes import root, collatz
 
@@ -8,7 +9,33 @@ app = FastAPI()
 
 app.include_router(root.router)
 
+@app.get("/put")
+def root(request:Request):
+    client_host = request.client.host
+    client_port = request.client.port
+    time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    file = open('Data.log','a')
+    data = "IP:"+str(client_host)+" port:"+str(client_port)+" Time:"+time+"\n"
+    file.write(data)
+    file.close()    
+    return{"message":"Data logged"}
+
+@app.get("/get")
+def root():
+    file = open("Data.log","r")
+    data = file.readlines()
+    file.close()
+    return data
+
+@app.get("/log/delete")
+def root():
+    file = open('Data.log','w')
+    file.close()    
+    return {"message":"data cleared"}
+
+
 app.include_router(collatz.router, prefix="/collatz", tags=["Collatz tag"])
+
 
 
 # Display message when the webservice is invoked without uvicorn.
